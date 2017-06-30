8fda6c292f687c5794249f063adb348ff17ee00b
Rivo Laks <code@rivolaks.com>
2017-06-30 11:35:54 +0300

    Ensure that our Docker networks config remains as it should be
    Specifically, we need to add nginx and postgres containers to corresponding networks of each docker-compose project. The new helper ensures that they get re-added when nginx/postgres containers are e.g. recreated.

--- eveli/Eveli.py
+++ eveli/Eveli.py
@@ -1,7 +1,7 @@
 import json
 import logging.config
-import subprocess
 
+import requests
 from docker import Client
 
 
@@ -28,6 +28,40 @@ logging.config.dictConfig({
 })
 
 
+def get_image_name(container_from):
+    """ Extracts the image name (without tag) from container's 'from' field. """
+    return container_from.rsplit(':', 1)[0]
+
+
+def get_compose_projects(cli):
+    """ Returns list of names of active Docker Compose projects
+    """
+    projects = set()
+    for container in cli.containers():
+        labels = container['Labels']
+        project = labels.get('com.docker.compose.project')
+        if project:
+            projects.add(project)
+
+    return projects
+
+
+def try_connect_container_to_network(cli, container, network):
+    try:
+        cli.connect_container_to_network(container, network)
+    except requests.exceptions.HTTPError:
+        pass
+
+
+def ensure_networks(cli):
+    projects = get_compose_projects(cli)
+    logging.info("ensure_networks(): docker-compose projects found: %s", projects)
+
+    for project in projects:
+        try_connect_container_to_network(cli, 'nginx', project + '_nginx')
+        try_connect_container_to_network(cli, 'postgres-9.5', project + '_postgres')
+
+
 def handler_nginx_reload(cli, event):
     """ Sends reload signal to nginx whenever a container's IP might have changed
     """
@@ -46,12 +80,34 @@ def handler_nginx_reload(cli, event):
         raise RuntimeError(exec_start)
 
 
+def handler_compose_networks(cli, event):
+    """ Ensures that nginx and postgres are in corresponding networks of each docker-compose project
+
+    They need to be re-added e.g. when nginx/postgres containers are restarted.
+    """
+
+    required_keys = ["Type", "Action", "from"]
+    actions = ["start", "unpause"]
+    image_names = ['nginx', 'postgres']
+
+    if not all(k in event for k in required_keys) or event['Action'] not in actions:
+        return
+    if get_image_name(event['from']) not in image_names:
+        return
+
+    logging.info("handler_compose_networks() %s %s %s", event['Action'], event['Type'], event['from'])
+    ensure_networks(cli)
+
+
 def listen():
     logging.info("Eveli is alive!")
 
     cli = Client(base_url='unix:///var/run/docker.sock')
 
-    event_handlers = [handler_nginx_reload]
+    # Just in case, run the networks-fixer at startup as well
+    ensure_networks(cli)
+
+    event_handlers = [handler_nginx_reload, handler_compose_networks]
 
     for event in cli.events():
         event = json.loads(event.decode('utf-8'))
