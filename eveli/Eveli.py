08552df5430f094435ce20a3c4602bf7575d1ed2
Rivo Laks <code@rivolaks.com>
2017-06-30 11:21:46 +0300

    Make Eveli a bit more generic, so that we can add multiple event handlers

--- eveli/Eveli.py
+++ eveli/Eveli.py
@@ -4,6 +4,7 @@ import subprocess
 
 from docker import Client
 
+
 logging.config.dictConfig({
     'version': 1,
     'formatters': {
@@ -26,26 +27,39 @@ logging.config.dictConfig({
     }
 })
 
-def listen():
-    logging.info("Eveli is alive!")
+
+def handler_nginx_reload(cli, event):
+    """ Sends reload signal to nginx whenever a container's IP might have changed
+    """
 
     reload_actions = ["start", "stop", "pause", "unpause", "kill"]
     required_keys = ["Type", "Action", "from"]
 
+    if not all(k in event for k in required_keys) or event['Action'] not in reload_actions:
+        return
+
+    logging.info("handler_nginx_reload() %s %s %s", event['Action'], event['Type'], event['from'])
+    exe = cli.exec_create(container='nginx', cmd='nginx -s reload')
+    exec_start = cli.exec_start(exec_id=exe)
+    inspect = cli.exec_inspect(exec_id=exe)
+    if 'ExitCode' in inspect and inspect['ExitCode'] != 0:
+        raise RuntimeError(exec_start)
+
+
+def listen():
+    logging.info("Eveli is alive!")
+
     cli = Client(base_url='unix:///var/run/docker.sock')
+
+    event_handlers = [handler_nginx_reload]
 
     for event in cli.events():
         event = json.loads(event.decode('utf-8'))
-        if all (k in event for k in required_keys) and event['Action'] in reload_actions:
-            logging.info("{action} {type} {container}".format(action=event['Action'], type=event['Type'], container=event['from']))
+        for handler in event_handlers:
             try:
-                exe = cli.exec_create(container='nginx', cmd='nginx -s reload')
-                exec_start = cli.exec_start(exec_id=exe)
-                inspect = cli.exec_inspect(exec_id=exe)
-                if 'ExitCode' in inspect and inspect['ExitCode'] != 0:
-                    raise RuntimeError(exec_start)
-            except Exception as e:
-                logging.exception("Failed to reload nginx!")
+                handler(cli, event)
+            except:
+                logging.exception("Event handler %s failed", handler.__name__)
 
 
 if __name__ == "__main__":
